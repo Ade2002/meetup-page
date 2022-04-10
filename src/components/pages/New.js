9c5ed0d8ddc6488d19b7fc0044a6dfeaef452e05
Ade2002 <sikirusulaimon2002@gmail.com>
2022-04-10 19:00:07 +0100

    added useNavigate instead of useHistory for latest react-router version hooks

--- src/components/pages/New.js
+++ src/components/pages/New.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 import NewMeetupForm from '../meetups/NewMeetupForm'
 const New = () => {
+    const history = useNavigate()
     const addMeetup = (meetupData) => {
-        fetch('https://meetups-7ed71-default-rtdb.firebaseio.com/meetuptable.json')
+        const url = 'https://meetups-7ed71-default-rtdb.firebaseio.com/meetuptable.json'
+        fetch(url,
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(() => {
+                history('/')
+            })
     }
     return (
         <section>
