9e4167edf405f34e14b4add7250e63c595e89814
Ade2002 <sikirusulaimon2002@gmail.com>
2022-04-10 13:21:15 +0100

    updated to react version 18 took out react dom and added create root

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Header } from "./layouts/Header";
+import './sass/style.scss'
 function App() {
   return (
     <div className="App">
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 export const Header = () => {
-  return ( <header>
-    <a href="/" class="logo">
-        <h1 width="34" height="43">LOGO</h1>
-    </a>
-    <nav class="navigate none">
-        <a href="/"> Tech Acad</a>
-        <a href="/"> QHSE</a>
-        <a href="/"> Stem Acad</a>
-        <a href="/"> Workstation </a>
-        <a href="/"> Contact </a>
-    </nav>
-    <span class="mobile">
-            <svg class="bar" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
-                clip-rule="evenodd">
-                <path d="M24 18v1h-24v-1h24zm0-6v1h-24v-1h24zm0-6v1h-24v-1h24z" fill="#1040e2" stroke="black"
-                    stroke-width="1.5"></path>
-                <path d="M24 19h-24v-1h24v1zm0-6h-24v-1h24v1zm0-6h-24v-1h24v1z"></path>
-            </svg>
-    </span>
-</header>
-  )
+    useEffect(() => {
+         document.title = 'hi'
+
+        })
+       const nav = document.querySelector('nav')
+        const headerFunction = () => {
+            nav.classList.toggle('none')
+        }
+ 
+    
+    return (
+        <header>
+            <a href="/" className="logo">
+                <h1 width="34" height="43">LOGO</h1>
+            </a>
+            <nav className="navigate none">
+                <a href="/"> Tech Acad</a>
+                <a href="/"> QHSE</a>
+                <a href="/"> Stem Acad</a>
+                <a href="/"> Workstation </a>
+                <a href="/"> Contact </a>
+            </nav>
+            <span className="mobile" onClick={headerFunction}>
+                <svg className="bar" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd"
+                    clipRule="evenodd">
+                    <path d="M24 18v1h-24v-1h24zm0-6v1h-24v-1h24zm0-6v1h-24v-1h24z" fill="#1040e2" stroke="black"
+                        strokeWidth="1.5"></path>
+                    <path d="M24 19h-24v-1h24v1zm0-6h-24v-1h24v1zm0-6h-24v-1h24v1z"></path>
+                </svg>
+            </span>
+        </header>
+    )
 }
