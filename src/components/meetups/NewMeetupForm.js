9d645defd88860e2f1e95982b5be4f33fb1066b7
Ade2002 <sikirusulaimon2002@gmail.com>
2022-04-10 17:37:43 +0100

    in the new meetup page started firebase to connect to a backend server and api

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -3,27 +3,41 @@ import Card from '../ui/Card'
 import classes from './NewMeetupForm.module.scss'
 const NewMeetupForm = () => {
     const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
     const submitMeetup = (e) => {
         e.preventDefault();
+        const titleValue = titleInputRef.current.value;
+        const imageValue = imageInputRef.current.value;
+        const addressValue = addressInputRef.current.value;
+        const descriptionValue = descriptionInputRef.current.value;
+        const meetupData = {
+            title: titleValue,
+            image: imageValue,
+            address: addressValue,
+            description: descriptionValue
+        }
+        console.log(meetupData)
     }
     return (
         <Card>
             <form className={classes.form} onSubmit={submitMeetup}>
                 <span className={classes.control}>
                     <label htmlFor='title'>Meetup Title</label>
-                    <input id='title' type={'text'} required />
+                    <input id='title' type={'text'} required ref={titleInputRef} />
                 </span>
                 <span className={classes.control}>
                     <label htmlFor='image'>Meetup Image</label>
-                    <input id='image' type={'url'} required />
+                    <input id='image' type={'url'} required ref={imageInputRef} />
                 </span>
                 <span className={classes.control}>
                     <label htmlFor='address'>Address</label>
-                    <input id='address' type={'text'} required />
+                    <input id='address' type={'text'} required ref={addressInputRef}/>
                 </span>
                 <span className={classes.control}>
                     <label htmlFor='description'>Description</label>
-                    <textarea id='description' rows={5} required />
+                    <textarea id='description' rows={5} required ref={descriptionInputRef}/>
                 </span>
                 <button className={classes.actions}>Add Meetup</button>
             </form>
